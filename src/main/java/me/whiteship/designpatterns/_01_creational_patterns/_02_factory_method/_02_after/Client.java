30c20519edcc610ff01b5b0c50a772a619f55d4f
Lee Myeongsang <mslee0052@gmail.com>
2022-12-03 12:11:39 +0900

    commit

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_02_factory_method/_02_after/Client.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_02_factory_method/_02_after/Client.java
@@ -4,11 +4,12 @@ public class Client {
     public static void main(String[] args) {
         Client client = new Client();
 
-        Ship whiteship = new WhiteShipFactory().orderShip("Whiteship", "keesun@mail.com");
+        Ship whiteship = new WhiteShipFactory().orderShip("whiteship", "keesun@mail.com");
         System.out.println(whiteship);
 
-//        Ship blackship = WhiteShipFactory.orderShip("Blackship", "keesun@mail.com");
-//        System.out.println(blackship);
+        Ship blackship = new BlackShipFactory().orderShip("blackship", "keesun@mail.com");
+        System.out.println(whiteship);
+
     }
 
 }
