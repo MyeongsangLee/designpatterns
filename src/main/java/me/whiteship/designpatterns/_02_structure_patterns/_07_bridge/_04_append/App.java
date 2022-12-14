703c46e30596b92ca7db289d521e99b60a6387b9
Lee Myeongsang <mslee0052@gmail.com>
2022-12-14 17:38:14 +0900

    commit

--- src/main/java/me/whiteship/designpatterns/_02_structure_patterns/_07_bridge/_04_append/App.java
+++ src/main/java/me/whiteship/designpatterns/_02_structure_patterns/_07_bridge/_04_append/App.java
@@ -2,6 +2,6 @@ package me.whiteship.designpatterns._02_structure_patterns._07_bridge._04_append
 
 public class App {
     public static void main(String[] args) {
-        new Rectangle(new RectDrawing());
+        Shape rectangle = new Rectangle(new RectDrawing());
     }
 }
