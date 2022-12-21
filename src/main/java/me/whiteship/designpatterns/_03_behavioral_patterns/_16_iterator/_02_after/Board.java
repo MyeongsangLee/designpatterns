76a019e8d6953f5ea6b63a0a0d4d95b732f361e4
Lee Myeongsang <mslee0052@gmail.com>
2022-12-22 06:10:09 +0900

    이터레이터 실습

---
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_16_iterator/_02_after/Board.java
@@ -0,0 +1,22 @@
+package me.whiteship.designpatterns._03_behavioral_patterns._16_iterator._02_after;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Getter @Setter
+public class Board {
+
+    List<Post> posts = new ArrayList<>();
+
+    public void addPost(String content) {
+        this.posts.add(new Post(content));
+    }
+
+    public Iterator<Post> getRecentPostIterator() {
+        return new RecentPostIterator(posts);
+    }
+}
---
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_16_iterator/_02_after/Post.java
@@ -0,0 +1,19 @@
+package me.whiteship.designpatterns._03_behavioral_patterns._16_iterator._02_after;
+
+import lombok.Getter;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Service
+public class Post {
+
+    private String title;
+    private LocalDateTime createDateTime;
+
+    public Post(String title) {
+        this.title = title;
+        this.createDateTime = LocalDateTime.now();
+    }
+}
