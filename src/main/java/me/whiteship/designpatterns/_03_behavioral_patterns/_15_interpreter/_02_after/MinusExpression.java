e8617ab97305d4ccb5ebc727285d6a257c5beaaa
Lee Myeongsang <mslee0052@gmail.com>
2022-12-22 09:16:21 +0900

    인터프리터 재복습

--- src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_15_interpreter/_02_after/MinusExpression.java
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_15_interpreter/_02_after/MinusExpression.java
@@ -2,18 +2,17 @@ package me.whiteship.designpatterns._03_behavioral_patterns._15_interpreter._02_
 
 import java.util.Map;
 
-
-public class PlusPostfixExpression implements PostfixExpression{
+public class MinusExpression implements PostfixExpression{
 
     private PostfixExpression left, right;
 
-    public PlusPostfixExpression(PostfixExpression left, PostfixExpression right) {
+    public MinusExpression(PostfixExpression left, PostfixExpression right) {
         this.left = left;
         this.right = right;
     }
 
     @Override
     public int interpret(Map<Character, Integer> context) {
-        return left.interpret(context);
+        return left.interpret(context) - right.interpret(context);
     }
 }
--- src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_15_interpreter/_03_after/PostfixExpression.java
+++ src/main/java/me/whiteship/designpatterns/_03_behavioral_patterns/_15_interpreter/_03_after/PostfixExpression.java
@@ -7,14 +7,20 @@ public interface PostfixExpression {
     int interpret(Map<Character, Integer> context);
 
     static PostfixExpression plus(PostfixExpression left, PostfixExpression right) {
-        return context -> left.interpret(context) + right.interpret(context);
+        return new PostfixExpression() {
+            @Override
+            public int interpret(Map<Character, Integer> context) {
+                return left.interpret(context) + right.interpret(context);
+            }
+        };
     }
 
     static PostfixExpression minus(PostfixExpression left, PostfixExpression right) {
         return context -> left.interpret(context) - right.interpret(context);
     }
 
-    static PostfixExpression variable(Character c) {
-        return context -> context.get(c);
+    static PostfixExpression variable(Character character) {
+        return context -> context.get(character);
     }
+
 }
